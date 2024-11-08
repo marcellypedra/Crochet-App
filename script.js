c802a0d02e4acc0517b3685158dd68f1f29a7be5
marcellypedra <20040674@mydbs.ie>
2024-11-08 08:01:22 +0000

    new pages for material list and projects

--- script.js
+++ script.js
@@ -3,35 +3,25 @@ document.addEventListener("DOMContentLoaded", function() { // # HEADER and FOOTE
     const headerHTML = `
             <section class="header">
             <div class="btnNavDiv">
-                <a href="index.html"><img src="image/logo.png" alt="Logo" class="logo" id="logo"></a>
+                <a href="index.html"><img src="img/yarn.png" alt="Logo" class="logo" id="logo"></a>
                 <nav id="menu" class="navbar navbar-expand-lg navbar-light">
                     <a id="nome" class="navbar-brand" href="#"></a>
-                    <button id="btnToogle" class="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="nav-container collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="nav-list navbar-nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="mortgage.html">Mortgage</a></li>
-                            <li class="nav-item"><a id="btnSell" class="nav-link" href="#Sell">Sell</a></li>
+                            <ul class="nav-list navbar-nav ml-auto">
+                            <li class="nav-item"><a class="nav-link" href="project.html">My Projects</a></li>
+                            <li class="nav-item"><a class="nav-link" href="material.html">My Material list</a></li>
                         </ul>
                     </div>
                 </nav>
             </div>
-                <h3>${currentFile === "index.html" ? "Welcome to BM Real Estate!" : "BM Real Estate Announcements"}</h3>
-            <div class="searchDiv">
-                <input type="text" id="searchInput" placeholder="Search...">
-                <input id="searchTerm" type="button" value="Search" onclick="searchTerm()"></li>
+                <h3>${currentFile === "index.html" ? "Welcome to Your Yarn.com!" : "Your Yarn.com"}</h3>
+                               
             </div>
             </section> `;
     const footerHTML = `
         <section class="footer">
             <div>
-                <p>Since ${new Date().getFullYear()} helping you to find your <b>H</b>ome <b>S</b>weet <b>H</b>ome</p>
+                <p>Since ${new Date().getFullYear()} helping you to knit your dreams! <b>H</b>ome <b>S</b>weet <b>H</b>ome</p>
                 <p>&copy; ${new Date().getFullYear()} My Website. All rights reserved.</p>
-            </div>
-            <div>
-                <p>Brenda Lopes</p>
-                <a href="mailto:20058225@mydbs.ie" target="_blank">20058225@mydbs.ie</a> 
             </div>
             <div>
                 <p>Marcelly Pedra</p> 
@@ -39,9 +29,7 @@ document.addEventListener("DOMContentLoaded", function() { // # HEADER and FOOTE
             </div>
         </section>`;
         
-    /*document.getElementById("showHeader").innerHTML = headerHTML;
-    document.getElementById("showFooter").innerHTML = footerHTML;*/
-
+    
     // Insert content
     const headerContainer = document.getElementById("showHeader");
     if (headerContainer) headerContainer.innerHTML = headerHTML;
