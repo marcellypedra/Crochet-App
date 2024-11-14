e292a5bf47584d9a367f49db1d9fd61f995ca740
marcellypedra <20040674@mydbs.ie>
2024-11-14 12:08:38 +0000

    deletedslideshow from script

--- script.js
+++ script.js
@@ -40,19 +40,9 @@ document.addEventListener("DOMContentLoaded", function() { // # HEADER and FOOTE
     if (footerContainer) footerContainer.innerHTML = footerHTML;
 });
 
-// automatic slideshow
 
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+
+
+
+
