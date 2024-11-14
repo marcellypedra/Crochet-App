8d5d69d4c87548791265e326b24bee56703f2dbe
marcellypedra <20040674@mydbs.ie>
2024-11-14 11:20:51 +0000

    header and footer style
    header and footer style

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() { // # HEADER and FOOTER
     const currentFile = window.location.pathname.split("/").pop();
     const headerHTML = `
-            <section class="header">
-            <div class="logo-container">
-            <a href="#" onclick="openPage('index')"><img src="img/logo.jpeg" alt="MyYarn" class="icons" id="logo"></a>
+<div class="header">
+    <div class="headercontainer">
+        <div class="headercontent">
+            <div class="headertext">
+                <h3 id="headertitle">${currentFile === "index.html" ? "Welcome to Your Yarn.com!" : "Your Yarn.com"}</h3>
+            </div>  
+            <div class="logocontainer">  
+                <a href="#" onclick="openPage('index')"><img src="img/logo.jpeg" alt="MyYarn" class="icons" id="logo"></a>
             </div>
-            <div class="btnNavDiv">
-                <nav id="menu" class="navbar navbar-expand-lg navbar-light">
-                    <a id="nome" class="navbar-brand" href="#"></a>
-                            <ul class="nav-list navbar-nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="project.html">My Projects</a></li>
-                            <li class="nav-item"><a class="nav-link" href="material.html">My Material list</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-                <h3>${currentFile === "index.html" ? "Welcome to Your Yarn.com!" : "Your Yarn.com"}</h3>
-                               
-            </div>
-            </section> `;
+        </div>
+    </div>
+            <nav class="headermenu">
+                <ul class=navlist> 
+                <li class="navitem"><a class="nav-link" href="project.html">My Projects</a></li>
+                <li class="navitem"><a class="nav-link" href="material.html">My Material list</a></li>
+                </ul>
+            </nav>
+</div>
+`;
     const footerHTML = `
         <section class="footer">
             <div>
