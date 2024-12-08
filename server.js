125f7a4a0919af91fb04e9eef811aee3791a7c73
marcellypedra <20040674@mydbs.ie>
2024-12-08 20:12:01 +0000

    adapting file to export express for testing

--- server.js
+++ server.js
@@ -261,5 +261,7 @@ app.patch('/api/reset-password', async (req, res) => {
     }
 });
 
+if (require.main === module)
+    app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+module.exports = app;
